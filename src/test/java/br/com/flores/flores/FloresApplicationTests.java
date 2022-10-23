ffe5d5dc25fff8baa5bfcd8063524d1f6a4ff285
cristo_ <cristiano.henriqueferreirabp@gmail.com>
2022-10-23 19:56:57 -0300

    initial commit

---
+++ src/test/java/br/com/flores/flores/FloresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.flores.flores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
